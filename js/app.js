0ff43eca2ffd49e47447916653cefea16cc3d58b
sergei kuksov <sergey.kuksov@func.ru>
2019-03-06 01:02:36 +0300

    Добавил файл конфига. Прописал скрипты.

--- js/app.js
+++
@@ -1,5 +0,0 @@
-import {someVariable as some1} from './module-1.js'
-import name from './module-2.js'
-
-console.log(some1);
-console.log(name);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {someVariable as some1} from './modules/module-1.js'
+import name from './modules/module-2.js'
+
+console.log(some1);
+console.log(name);
